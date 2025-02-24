b2c172550e9e00488eb9bb8c67e289f94dad5eb6
Ovinda Rathnayeka <151762661+Ovinda-Rathnayeka@users.noreply.github.com>
2025-02-24 15:18:11 +0530

    frontend and backend

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import image from "./images/Screenshot 2025-02-14 192651.png";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center p-4 bg-[#0a181f]">
+      <div className="flex items-center">
+        <img src={image} alt="SKW Photography" className="h-12" />
+      </div>
+      <div className="hidden md:flex space-x-8">
+        <a
+          href="#home"
+          className="text-[#8f8888] font-bold hover:text-[#747c7c]"
+        >
+          Home
+        </a>
+        <a
+          href="#about"
+          className="text-[#8f8888] font-bold hover:text-[#747c7c]"
+        >
+          About Us
+        </a>
+        <a
+          href="#video"
+          className="text-[#8f8888] font-bold hover:text-[#747c7c]"
+        >
+          Video Gallery
+        </a>
+        <a
+          href="#packages"
+          className="text-[#8f8888] font-bold hover:text-[#747c7c]"
+        >
+          Packages
+        </a>
+        <a
+          href="#store"
+          className="text-[#8f8888] font-bold hover:text-[#747c7c]"
+        >
+          Store
+        </a>
+        <a
+          href="#contact"
+          className="text-[#8f8888] font-bold hover:text-[#747c7c]"
+        >
+          Contact Us
+        </a>
+      </div>
+      <div className="flex space-x-4">
+        <button className="bg-[#73747c] text-white px-4 py-2 rounded hover:bg-[#3c4c51]">
+          Login
+        </button>
+        <button className="bg-[#5b5c5e] text-white px-4 py-2 rounded hover:bg-[#30383d]">
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
