38d6da253e3df1b859e97d6685cce3161b490ad6
Kavishka Malshan <kavishka.malshan2001@gmail.com>
2025-05-03 22:38:16 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,19 +12,9 @@ import AddFeedback from "./pages/AddFeedback.js";
 import Product from "./pages/ProductDisplay.jsx";
 import Cart from "./pages/CartDisplay.js";
 import UpdateFeedback from "./pages/UpdateFeedback.js";
-<<<<<<< HEAD
-
-=======
-import Product from "./pages/ProductDisplay.jsx";
-import Cart from "./pages/CartDisplay.js";
 import DisplayRental from "./pages/DisplayRental.jsx";
->>>>>>> 7d12eb9e0b32ab7cd105bf9da73ab1fa28988c42
 import Profile from "./pages/ProfilePage.jsx";
-import BookingHistory from "./pages/BookingPage.jsx";
-
-import Product from "./pages/ProductDisplay.jsx";
-import Cart from "./pages/CartDisplay.js";
-import DisplayRental from "./pages/DisplayRental.jsx";
+import BookingHistory from "./pages/BookignHistory.jsx";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -49,27 +39,19 @@ function App() {
       <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/Home" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/packages" element={<PackagePage />} />
         <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/signup" element={<SignupPage setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/feedbacks" element={<Feedbacks />} />
         <Route path="/add-feedback" element={<AddFeedback />} />
+        <Route path="/feedbacks/:id" element={<UpdateFeedback />} />
         <Route path="/product" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/feedbacks/:id" element={<UpdateFeedback />} />
-<<<<<<< HEAD
-
-=======
-        <Route path="/product" element={<Cart />} />
         <Route path="/rental" element={<DisplayRental />} />
->>>>>>> 7d12eb9e0b32ab7cd105bf9da73ab1fa28988c42
         <Route path="/profile" element={<Profile />} />
         <Route path="/booking-history" element={<BookingHistory />} />
-=======
-        <Route path="/product" element={<Cart />} />
-        <Route path="/rental" element={<DisplayRental/>}/>
       </Routes>
     </Router>
   );
